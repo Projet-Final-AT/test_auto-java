5fca718e7e451b61b95edaffdf34a5c2d3d5ece7
Martine <martine.test@orange.fr>
2021-01-21 13:48:01 +0100

    test

---
+++ src/test/java/TestLancementChrome.java
@@ -0,0 +1,36 @@
+package test.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestLancementChrome {
+
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        String s = System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void VerifTest() throws InterruptedException {
+        //lancement du site
+        System.out.println("Lancement de Orange HRM");
+        driver.get("https://opensource-demo.orangehrmlive.com");
+        Thread.sleep(5000);
+    }
+
+
+     @AfterClass
+     public static void TearDown() {
+         //Fermeture du navigateur et clôture de l'instance
+         driver.quit();
+     }
+
+}
