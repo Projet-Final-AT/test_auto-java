621e17f05315373634889ffb5f8a0ae6672ed9e1
Martine <martine.test@orange.fr>
2021-01-21 11:30:42 +0100

    Ajout de commentaire

---
+++ src/test/java/TestConnexion.java
@@ -0,0 +1,28 @@
+package test.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class TestConnexion {
+
+    @Test
+    public void setUp() throws Exception
+    {
+        //Ajout du chemin driver
+        String s = System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+        //Initialisation de l'instance du driver
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //Démarrage du navigateur
+        driver.get("https://google.fr");
+    }
+
+//commentaire
+
+
+}
+
+
